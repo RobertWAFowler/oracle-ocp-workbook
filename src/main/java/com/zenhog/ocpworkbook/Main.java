a93117189cbdaa02cc363307948b20152f50c974
RobertWAFowler <rfowler.reg@gmail.com>
2019-07-05 12:28:45 +0200

    Dev Commit

---
+++ src/main/java/com/zenhog/ocpworkbook/Main.java
@@ -0,0 +1,7 @@
+package com.zenhog.ocpworkbook;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/zenhog/ocpworkbook/MainTest.java
@@ -0,0 +1,10 @@
+package com.zenhog.ocpworkbook;
+
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void nothing(){}
+
+}
